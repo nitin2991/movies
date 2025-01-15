09100e8b93cab69c8e109be72a41ac59b04805f5
nitin2991 <nitin.kamboj510@gmail.com>
2025-01-15 20:26:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,13 +13,13 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
+API_ID = int(environ.get('API_ID', '29801489'))
+API_HASH = environ.get('API_HASH', '74aaff69f6cacf1239d6a6f38a24374c')
 BOT_TOKEN = environ.get('BOT_TOKEN', '8198129218:AAFo2rKp8TZjEWcmpMb9GhfIBu-ACAs17YE')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5665677555').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Movies_k001") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002227710994'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
